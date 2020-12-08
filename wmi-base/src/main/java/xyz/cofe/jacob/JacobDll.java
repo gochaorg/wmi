90d27fdff3ae619f78f0fd5c0984b213b1a363e7
Kamnev Georgiy <nt.gocha@gmail.com>
2020-12-09 02:01:12 +0500

    sys props for init dll

--- wmi-base/src/main/java/xyz/cofe/jacob/JacobDll.java
+++ wmi-base/src/main/java/xyz/cofe/jacob/JacobDll.java
@@ -7,6 +7,20 @@ import java.io.*;
 import java.net.URL;
 import java.util.logging.Logger;
 
+/**
+ * Копирует jacob-xxxx.dll и устанавливает системные переменные чтоб указывали на оные dll
+ * <ul>
+ *     <li> Переменная <b>xyz.cofe.jacob.skipInit</b> <code>= true</code> - не производит инициализацию
+ *     </li>
+ *     <li> Переменная <b>jacob.dll.name.x86</b> определена - не производит инициализацию
+ *     </li>
+ *     <li> Переменная <b>jacob.dll.name.x64</b> определена - не производит инициализацию
+ *     </li>
+ *     <li>
+ *         <b>xyz.cofe.jacob.installDir</b> - указывает на каталог, куда копировать dll
+ *     </li>
+ * </ul>
+ */
 public class JacobDll {
     private static volatile boolean inited = false;
     private static final String x64dllName = "jacob-1.20-x64.dll";
@@ -19,22 +33,69 @@ public class JacobDll {
         ){
             IOFun.copy(sfrom,sto);
             sto.flush();
+            sto.close();
         }
     }
 
+    /**
+     * Проверяет наличие системных свойств, в зависимости от наличия/значения завершает инициализацию:
+     * @return
+     * <ul>
+     *     <li> Переменная <code>xyz.cofe.jacob.skipInit = true</code> - не производит инициализацию
+     *     </li>
+     *     <li> Переменная <code>jacob.dll.name.x86</code> определена - не производит инициализацию
+     *     </li>
+     *     <li> Переменная <code>jacob.dll.name.x64</code> определена - не производит инициализацию
+     *     </li>
+     * </ul>
+     */
+    public static boolean skipInit(){
+        boolean skipInit = System.getProperty("xyz.cofe.jacob.skipInit","false").equalsIgnoreCase("true");
+        if( skipInit )return true;
+
+        String dllName = System.getProperty( "jacob.dll.name" );
+        if( dllName!=null && dllName.trim().length()>0 )return true;
+
+        String dllNameX86 = System.getProperty( "jacob.dll.name.x86" );
+        if( dllNameX86!=null && dllNameX86.trim().length()>0 )return true;
+
+        String dllNameX64 = System.getProperty( "jacob.dll.name.x64" );
+        if( dllNameX64!=null && dllNameX64.trim().length()>0 )return true;
+
+        return false;
+    }
+
+    @SuppressWarnings("ConstantConditions")
     synchronized public static void init(){
         if( inited )return;
-        String tmpdirPath = System.getProperty("java.io.tmpdir");
-        if( tmpdirPath==null ){
-            throw new Error("can't read sys property: java.io.tmpdir");
+        if( skipInit() ){
+            inited = true;
+            return;
         }
 
-        File tmpdir = new File(tmpdirPath);
-        if( !tmpdir.isDirectory() ){
-            throw new Error("java.io.tmpdir(=$tmpdir) is not directory");
+        String installDllTo = System.getProperty("xyz.cofe.jacob.installDir");
+
+        String tmpDirPath = System.getProperty("java.io.tmpdir");
+
+        String installDirPath =
+            installDllTo!=null && installDllTo.trim().length()>0 ?
+                installDllTo :
+                tmpDirPath;
+
+        if( installDirPath==null ){
+            if( tmpDirPath==null ){
+                throw new Error("can't read sys property: java.io.tmpdir");
+            }else {
+                throw new Error("can't define install dll dir");
+            }
         }
 
-        File x64dll = new File(tmpdir,x64dllName);
+        File installDir = new File(installDirPath);
+        if( !installDir.isDirectory() ){
+            throw new Error(installDir+" is not directory");
+        }
+
+        File x64dll = new File(installDir,x64dllName);
         if( !x64dll.exists() ){
             String res = "/jacob-dll/"+x64dllName;
             URL x64dllUrl = JacobDll.class.getResource(res);
@@ -51,12 +112,12 @@ public class JacobDll {
             logger.info("copied "+x64dllUrl+" to "+x64dll);
         }
 
-        File x86dll = new File(tmpdir,x86dllName);
+        File x86dll = new File(installDir,x86dllName);
         if( !x86dll.exists() ){
             String res = "/jacob-dll/"+x86dllName;
             URL x86dllUrl = JacobDll.class.getResource(res);
             if( x86dllUrl==null ){
-                throw new Error("resource $res not found");
+                throw new Error("resource "+res+" not found");
             }
 
             try{
